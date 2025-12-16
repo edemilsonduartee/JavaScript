0d6121755c8f43a69dc67545221d88d614a07cc6
edemilsonduartee <edemilson_melo@hotmail.com>
2025-12-16 02:39:28 -0400

    add objects

--- variaveis/tiposDeDados.js
+++ variaveis/tiposDeDados.js
@@ -9,11 +9,13 @@
     - array -> array
 
 */
+/*
 const myAge = 30
 
 const myString = `Minha idade é ${myAge}` // Template literals 
 
 console.log(myString)
+
 
 //outros tipos de dados number
 
@@ -23,19 +25,36 @@ const isStudent = true // boolean, pode ser verdadeiro ou falso
 const emptyValue = null // null, representa a ausência de valor
 let notDefined // undefined, variável declarada mas não inicializada
 
-
+*/
 // outro tipo de dados boolean
 
 //boolean é usado para representar valores verdadeiros ou falsos, geralmente em operações de comparação ou controle de fluxo
 
 true // verdadeiro
 false // falso
-
+/*
 //exemplos de boolean
 const isAdult = myAge >= 18 // true, porque 30 é maior que 18
 const isMinor = myAge < 18 // false, porque 30 não é menor que 18
 console.log("É adulto?", isAdult)
 console.log("É menor de idade?", isMinor)
-
+*/
 
 //outro tipo de dados object e array
+const name = "Edemilson"
+const age = 25
+const anddress = "Rua Mario Palma,438"
+
+const edemilson = { 
+    name: "Edemilson",
+    age: 25,
+    anddress: "Rua Mario Palma,438"
+
+}
+
+console.log(edemilson.anddress) // acessando a propriedade 'anddress' do objeto 'edemilson'
+
+// outros tipos de dados Null e Undefined
+
+let emptyValue = null // null, representa a ausência de valor
+let notDefined // undefined, variável declarada mas não inicializada    
