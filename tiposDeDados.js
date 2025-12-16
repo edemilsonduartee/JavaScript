28a440843bbe8ce914cb74874f121fa6bd5e1a45
edemilsonduartee <edemilson_melo@hotmail.com>
2025-12-16 01:40:03 -0400

    Aulas Js

---
+++ tiposDeDados.js
@@ -0,0 +1,16 @@
+/*string
+
+    - string -> texto -> colocar aspas duplas ou simplese tambem usar a crase (``) para template literals
+    - number -> número -> 
+    - boolean -> verdadeiro ou falso
+    - null -> nulo
+    - undefined -> indefinido
+    - object -> objeto
+    - array -> array
+
+*/
+const myAge = 30
+
+const myString = `Minha idade é ${myAge}` // Template literals 
+
+console.log(myString)
---
+++ variaveis/variaveis.js
@@ -0,0 +1,23 @@
+//conceitos de variáveis em JavaScript
+//variaveis separa um espaço de memoria para armazenar dados
+
+    /*
+        -var -> Descontinuado / nao usar
+        -let -> valor pode ser declado novamente
+        -const -> valor NAO pode ser decladao novamente
+    */
+
+var nome = "João"; // variável global ou de função
+let idade = 25; // variável de bloco
+const pi = 3.14; // constante de bloco
+
+
+
+//-------------------------------------------------------------------------//
+console.log("Nome:", nome);// exibindo valores no console
+console.log("Idade:", idade);//
+console.log("Valor de pi:", pi);// exibindo valores no console
+console.log("Olá, mundo!");// exibindo uma mensagem no console
+idade = 26; // alterando o valor da variável 'idade'
+console.log("Idade atualizada:", idade);
+ 
