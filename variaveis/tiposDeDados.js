1723c7aed630a55e3834b71215e5f8b00620de09
edemilsonduartee <edemilson_melo@hotmail.com>
2025-12-16 01:57:36 -0400

    Add boolean

--- variaveis/tiposDeDados.js
+++ variaveis/tiposDeDados.js
@@ -15,9 +15,27 @@ const myString = `Minha idade é ${myAge}` // Template literals
 
 console.log(myString)
 
-//outros tipos de dados  
+//outros tipos de dados number
+
 
 const number1 = 3.14 // number, posso usar para somar, dividir, multiplicar e subtrair
 const isStudent = true // boolean, pode ser verdadeiro ou falso
 const emptyValue = null // null, representa a ausência de valor
 let notDefined // undefined, variável declarada mas não inicializada
+
+
+// outro tipo de dados boolean
+
+//boolean é usado para representar valores verdadeiros ou falsos, geralmente em operações de comparação ou controle de fluxo
+
+true // verdadeiro
+false // falso
+
+//exemplos de boolean
+const isAdult = myAge >= 18 // true, porque 30 é maior que 18
+const isMinor = myAge < 18 // false, porque 30 não é menor que 18
+console.log("É adulto?", isAdult)
+console.log("É menor de idade?", isMinor)
+
+
+//outro tipo de dados object e array
