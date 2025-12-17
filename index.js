02a15f1ca967489a1a966b20ba8495ed5d22da9f
edemilsonduartee <edemilson_melo@hotmail.com>
2025-12-17 01:45:30 -0400

    add altereção de textos HTML e seletores CSS

--- index.js
+++ index.js
@@ -10,5 +10,24 @@ console.log(input) // exibindo o elemento de input no console
 //const elements = document.getElementsByName("nome-completo"); 
 //console.log(elements) // exibindo os elementos com o atributo name "nome-completo" no console
 
-const elements = document.querySelector("p");
-console.log(elements) // exibindo o primeiro elemento de parágrafo no console
+//const elements = document.querySelector("p");
+//console.log(elements) // exibindo o primeiro elemento de parágrafo no console
+
+
+//const elements = document.querySelectorAll("p");
+//console.log(elements) // exibindo todos os elementos de parágrafo no console 
+
+
+
+//__________________________________________________________________________
+
+/*
+ console.log(Element.textContent); // exibindo o texto do título no console
+ console.log(Element.innerHTML); // exibindo o HTML interno do título no console, leva em conta as tags HTML
+ console.log(Element.innerText); // exibindo o texto visível do título no console, leva em conta o estilo CSS
+ */
+//__________________________________________________________________________
+
+const button = document.querySelector("h1");
+
+button.style.color = "blue";  // alterando a cor do texto do título para azul
