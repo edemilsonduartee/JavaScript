d8f648f12effd85756057cbf319614bfca29a462
edemilsonduartee <edemilson_melo@hotmail.com>
2025-12-26 02:02:51 -0400

    add operação atribuição

---
+++ JavaScript/OpAtribuicao.js
@@ -0,0 +1,25 @@
+/* 
+
+    Operadores de Atribuição:
+
+    = -> Atribuição
+    += -> Atribuição de Soma
+    -= -> Atribuição de Subtração
+    *= -> Atribuição de Multiplicação
+    /= -> Atribuição de Divisão 
+
+    operadores de Atribuiçao, sao usados para atribuir 
+    valores a variaveis e realizar operacoes matematicas 
+    junto com o  resultdo da atribuicao  (conhecidos como muqiranas dos codigos)
+
+
+*/
+
+let result = 30
+
+result += 10 // result = result + 10
+result -= 5 // result = result - 5
+result *= 2 // result = result * 2
+result /= 3 // result = result / 3
+
+console.log(result)
