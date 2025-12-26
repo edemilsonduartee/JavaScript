2edb1162cadfcc89a598a28f698c5abcbe780e8d
edemilsonduartee <edemilson_melo@hotmail.com>
2025-12-26 02:15:41 -0400

    add operadores de comparação

---
+++ JavaScript/OpComparação.js
@@ -0,0 +1,22 @@
+/* 
+
+    Operadores de comparação:
+
+    > maior que
+    < menor que
+    >= maior ou igual a
+    <= menor ou igual a
+    == igual a, compara o valor mas nao o tipo
+    != diferente de
+    === igual a (comparação estrita) compara  o valor e o tipo 
+
+
+*/
+
+const firstname = 20
+const lastname = 20
+
+console.log(firstname == lastname)
+console.log(firstname === lastname)
+console.log(firstname != lastname)
+console.log(firstname !== lastname)
