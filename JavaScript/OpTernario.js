6f9b2136cb3af9372d8f07c2e34310ea63c606b9
edemilsonduartee <edemilson_melo@hotmail.com>
2026-01-03 02:13:27 -0400

    add operador ternário

---
+++ JavaScript/OpTernario.js
@@ -0,0 +1,14 @@
+/* 
+ Operadores ternários
+
+    ? -> Se (if_)
+    : -> Senão (else)
+
+*/
+
+const idade = 17
+//const podeBeber = idade >= 18 ? "Pode beber" : "Nao pode beber"
+//console.log(podeBeber)          
+
+
+idade >= 18 ? console.log("Pode beber") : console.log("Nao pode beber");
